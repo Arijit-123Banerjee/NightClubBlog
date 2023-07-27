a11298c5f02c419adb1cf4c0f12d7bab1712a0e8
Arijit <arijitbanerjee873@gmail.com>
2023-07-27 15:50:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.main');
+ScrollReveal().reveal('.revel2', { duration: 600 }, { scale: 1 }, { reset: true });
+ScrollReveal().reveal('.revel3', { duration: 600 }, { scale: 1 }, { reset: true });
+ScrollReveal().reveal('.revel4', { duration: 900 }, { scale: 1 }, { reset: true });
+ScrollReveal().reveal('.revel5', { duration: 900 }, { distance: '50%' }, { reset: true });
+ScrollReveal().reveal('.revel6', { duration: 1200 }, { distance: '50%' }, { reset: true });
+ScrollReveal().reveal('.revel7', { duration: 1520 }, { distance: '100%' }, { reset: true });
+ScrollReveal().reveal('.revel8', { duration: 1520 }, { distance: '100%' }, { reset: true });
+ScrollReveal().reveal('.box', { duration: 1520 }, { distance: '100%' }, { reset: true });
